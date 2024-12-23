85d5e74ff7e64f5e14e15977c642b4eb368ce742
Trisha <johndoe@example.com>
2024-12-24 00:58:10 +0600

    weather real data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 function refreshWeather(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#tmp");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  let description = document.querySelector("#desc");
+  description.innerHTML = response.data.condition.description;
+
+  let humidity = document.querySelector("#humid");
+  humidity.innerHTML = response.data.temperature.humidity;
+
+  let windy = document.querySelector("#wind");
+  windy.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
